354eceb3086c3cb761f73d664f189733b45b70e7
SuperDasMan <bigdaddydas@gmail.com>
2022-01-19 00:08:09 -0700

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -72,17 +72,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+
   activate: function(event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     // array to store the task data in
@@ -90,10 +93,19 @@ $(".card .list-group").sortable({
 
     // loop over current set of children in sortable list
     $(this).children().each(function() {
-      var text = $(this)
+
+      // add task data to the temp array as an object
+      tempArr.push({
+      text: $(this)
       .find("p")
       .text()
-      .trim();
+      .trim(),
+      date: $(this)
+      .find("span")
+      .text()
+      .trim()
+    });
+  });
 
     // trim down list's ID to match object property
     var arrName = $(this)
@@ -103,20 +115,6 @@ $(".card .list-group").sortable({
     // update array on tasks object and save
     tasks[arrName] = tempArr;
       saveTasks();
-  
-    var date = $(this)
-      .find("span")
-      .text()
-      .trim();
-  
-    // add task data to the temp array as an object
-    tempArr.push({
-      text: text,
-      date: date
-    });
-  });
-
-  console.log(tempArr);
   }
 });
 
@@ -124,14 +122,18 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
-    ui.draggable.remove();;
+    ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
+});
+
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // modal was triggered
@@ -146,12 +148,8 @@ $("#task-form-modal").on("shown.bs.modal", function() {
   $("#modalTaskDescription").trigger("focus");
 });
 
-$("#modalDueDate").datepicker({
-  minDate: 1
-});
-
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -297,4 +295,9 @@ $("#remove-tasks").on("click", function() {
 // load saved tasks
 loadTasks();
 
+setInterval(function() {
+  $(".card .list-group-item").each(function() {
+    auditTask($(this));
+  });
+}, 1800000);
 
